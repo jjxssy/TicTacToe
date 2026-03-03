3fee30294ef39fffc49886f9f7d9932f3dc3b209
gripindor <134731974+gripindor@users.noreply.github.com>
2026-03-03 19:35:13 +0200

    3/3
    הוספת אינדיקציה לכמות קלפים בחבילה

--- Assets/_Project/Scripts/CardDeck.cs
+++ Assets/_Project/Scripts/CardDeck.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 using UnityEngine.EventSystems; // Required for clicking
 using System; // Required for Action
 using System.Collections.Generic;
+using TMPro;
 
-public class Deck : MonoBehaviour, IPointerClickHandler
+public class Deck : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public List<CardData> cards = new List<CardData>();
 
@@ -22,5 +23,18 @@ public class Deck : MonoBehaviour, IPointerClickHandler
             
             Debug.Log($"Deck: Broadcasted draw for {drawnData.name}");
         }
+    }
+
+    public TextMeshProUGUI deckCountText;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        deckCountText.text = $"cards {cards.Count}";
+        deckCountText.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        deckCountText.gameObject.SetActive(false);
     }
 }
